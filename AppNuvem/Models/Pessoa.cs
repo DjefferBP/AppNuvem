2277c2b8f31ba68b7b97edebc647d3ca4df05b0c
djeffer <djefferbervianprange123@gmail.com>
2026-03-04 22:16:02 -0300

    mudanças simples de interface e no model para exibição mais limpa dos dados

--- AppNuvem/Models/Pessoa.cs
+++ AppNuvem/Models/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AppNuvem.Models
@@ -11,5 +12,50 @@ namespace AppNuvem.Models
         public string celular { get; set;}
         public DateTime datanasc { get; set;}
         public string genero { get; set;}
+        public Color CorGenero
+        {
+            get
+            {
+                if (genero == "M")
+                    return Color.Blue;
+                else if (genero == "F")
+                    return Color.Pink;
+                else
+                    return Color.Gray;
+            }
+        }
+
+        public string GeneroDesc
+        {
+            get
+            {
+                switch(genero)
+                {
+                    case "M":
+                        return "Masculino";
+                    case "F":
+                        return "Feminino";
+                    default:
+                        return "Outro";
+                }
+            }
+        }
+
+        public string CelularFormatado
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(celular))
+                    return "";
+
+                if (celular.Length == 11)
+                    return $"({celular.Substring(0, 2)}) {celular.Substring(2, 5)}-{celular.Substring(7, 4)}";
+
+                if (celular.Length == 10)
+                    return $"({celular.Substring(0, 2)}) {celular.Substring(2, 4)}-{celular.Substring(6, 4)}";
+
+                return celular;
+            }
+        }
     }
 }
